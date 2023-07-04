a8b72cd7554082c4e934a591c832fb4cf6363065
davi <davigabtomaz@gmail.com>
2023-07-04 15:17:31 -0300

    Inicializado projeto, instalado Tailwind e Flowbite

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+module.exports = {
+  content: ["./index.html",
+   "./src/**/*.{vue,js,ts,jsx,tsx}", 
+   "./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("flowbite/plugin"),
+  ],
+}
+
