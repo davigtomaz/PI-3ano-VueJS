49e898104c6d2ab2b4871efa95768a442dd41e27
davi <davigabtomaz@gmail.com>
2023-07-07 12:59:23 -0300

    feito correções/adicões sidebar,button and cards

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,99 +1,164 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
   <div class="p-4 sm:ml-64">
-    <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-      <div class="grid grid-cols-3 gap-4 mb-4">   
-        <div class="flex items-center justify-center h-30 rounded bg-gray-50 dark:bg-gray-800">
-          <a
-            href="#"
-            class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
-          >
-            <img
-              class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-              src="@/assets/20103781.jpg"
-              alt=""
-            />
-            <div class="flex flex-col justify-between p-4 leading-normal">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
-              </h5>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse
-                chronological order.
-              </p>
+    <div class="p-4 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+      <div class="flex justify-start ">
+        <button class="btn btn-wide" onclick="my_modal_5.showModal()">Registrar Livro</button>
+        <dialog id="my_modal_5" class="modal modal-bottom sm:modal-middle">
+          <form method="dialog" class="modal-box">
+            <h3 class="font-bold text-lg">Hello!</h3>
+            <p class="py-4">Press ESC key or click the button below to close</p>
+            <div class="modal-action">
+              <!-- if there is a button in form, it will close the modal -->
+              <button class="btn">Close</button>
             </div>
-          </a>
+          </form>
+        </dialog>
+      </div>
+
+      <div class="grid grid-cols-3 gap-4 mb-4 mt-5">
+        <div class="flex items-center justify-center h-30 rounded bg-gray-50 dark:bg-gray-800">
+          <div class="card lg:card-side bg-base-100 shadow-xl">
+            <figure class="w-56">
+              <img src="@/assets/20103781.jpg" alt="Album" />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">New album is released!</h2>
+              <p>Click the button to listen on Spotiwhy app.</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Listen</button>
+              </div>
+            </div>
+          </div>
         </div>
         <div class="flex items-center justify-center h-30 rounded bg-gray-50 dark:bg-gray-800">
-          <a
-            href="#"
-            class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
-          >
-            <img
-              class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-              src="@/assets/20103781.jpg"
-              alt=""
-            />
-            <div class="flex flex-col justify-between p-4 leading-normal">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
-              </h5>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse
-                chronological order.
-              </p>
+          <div class="card lg:card-side bg-base-100 shadow-xl">
+            <figure class="w-56">
+              <img src="@/assets/20103781.jpg" alt="Album" />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">New album is released!</h2>
+              <p>Click the button to listen on Spotiwhy app.</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Listen</button>
+              </div>
             </div>
-          </a>
+          </div>
         </div>
         <div class="flex items-center justify-center h-30 rounded bg-gray-50 dark:bg-gray-800">
-          <a
-            href="#"
-            class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
-          >
-            <img
-              class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-              src="@/assets/20103781.jpg"
-              alt=""
-            />
-            <div class="flex flex-col justify-between p-4 leading-normal">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
-              </h5>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse
-                chronological order.
-              </p>
+          <div class="card lg:card-side bg-base-100 shadow-xl">
+            <figure class="w-56">
+              <img src="@/assets/20103781.jpg" alt="Album" />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">New album is released!</h2>
+              <p>Click the button to listen on Spotiwhy app.</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Listen</button>
+              </div>
             </div>
-          </a>
+          </div>
         </div>
         <div class="flex items-center justify-center h-30 rounded bg-gray-50 dark:bg-gray-800">
-          <a
-            href="#"
-            class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
-          >
-            <img
-              class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-              src="@/assets/20103781.jpg"
-              alt=""
-            />
-            <div class="flex flex-col justify-between p-4 leading-normal">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
-              </h5>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse
-                chronological order.
-              </p>
+          <div class="card lg:card-side bg-base-100 shadow-xl">
+            <figure class="w-56">
+              <img src="@/assets/20103781.jpg" alt="Album" />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">New album is released!</h2>
+              <p>Click the button to listen on Spotiwhy app.</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Listen</button>
+              </div>
             </div>
-          </a>
+          </div>
         </div>
-        
+        <div class="flex items-center justify-center h-30 rounded bg-gray-50 dark:bg-gray-800">
+          <div class="card lg:card-side bg-base-100 shadow-xl">
+            <figure class="w-56">
+              <img src="@/assets/20103781.jpg" alt="Album" />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">New album is released!</h2>
+              <p>Click the button to listen on Spotiwhy app.</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Listen</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="flex items-center justify-center h-30 rounded bg-gray-50 dark:bg-gray-800">
+          <div class="card lg:card-side bg-base-100 shadow-xl">
+            <figure class="w-56">
+              <img src="@/assets/20103781.jpg" alt="Album" />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">New album is released!</h2>
+              <p>Click the button to listen on Spotiwhy app.</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Listen</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="flex items-center justify-center h-30 rounded bg-gray-50 dark:bg-gray-800">
+          <div class="card lg:card-side bg-base-100 shadow-xl">
+            <figure class="w-56">
+              <img src="@/assets/20103781.jpg" alt="Album" />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">New album is released!</h2>
+              <p>Click the button to listen on Spotiwhy app.</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Listen</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="flex items-center justify-center h-30 rounded bg-gray-50 dark:bg-gray-800">
+          <div class="card lg:card-side bg-base-100 shadow-xl">
+            <figure class="w-56">
+              <img src="@/assets/20103781.jpg" alt="Album" />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">New album is released!</h2>
+              <p>Click the button to listen on Spotiwhy app.</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Listen</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="flex items-center justify-center h-30 rounded bg-gray-50 dark:bg-gray-800">
+          <div class="card lg:card-side bg-base-100 shadow-xl">
+            <figure class="w-56">
+              <img src="@/assets/20103781.jpg" alt="Album" />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">New album is released!</h2>
+              <p>Click the button to listen on Spotiwhy app.</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Listen</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="flex items-center justify-center h-30 rounded bg-gray-50 dark:bg-gray-800">
+          <div class="card lg:card-side bg-base-100 shadow-xl">
+            <figure class="w-56">
+              <img src="@/assets/20103781.jpg" alt="Album" />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">New album is released!</h2>
+              <p>Click the button to listen on Spotiwhy app.</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Listen</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-
-    
   </div>
-
 </template>
