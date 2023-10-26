484b1961173bb824152f8791bd23f13267d2f90f
davi <davigabtomaz@gmail.com>
2023-10-26 09:30:19 -0300

    mudança no card e adicionado loading

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,7 +2,10 @@
 import axios from 'axios';
 import { ref, onMounted } from 'vue';
 
+import Loading from '../components/Loading.vue';
+
 const livros = ref([]); 
+const loading = ref(true);
 
 const buscarLivros = async () => { 
   try {
@@ -10,6 +13,8 @@ const buscarLivros = async () => {
     livros.value = resposta.data; 
   } catch (erro) {
     console.error(erro);
+  } finally {
+    loading.value = false;
   }
 }
 
@@ -19,19 +24,164 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="grid grid-cols-3 gap-1 mb-6 mt-7  w-full">
-    <div v-for="livro in livros" :key="livro.id" class="flex items-center justify-center ">
-      <div class="card lg:card-side bg-base-100 shadow-xl">
-        <figure class="w-full h-full">
-          <img :src="livro.capa" alt="Capa do Livro" />
-        </figure>
-        <div class="card-body mr-1">
-          <h2 class="card-title">{{ livro.titulo || 'Título não disponível' }}</h2>
-          <p>Categoria: {{ livro.categoria.join(',') }}</p>
-          <p>Autor(s): {{ livro.autores.join(',') }}</p>
-          <p>Localização: {{ livro.localizacao }}</p>    
+  <div class="container">
+    <Loading v-if="loading" />
+    <div
+      class="card"
+      :style="{ '--clr': cardColor }"
+      v-for="(livro, index) in livros"
+      :key="index"
+    >
+      <div class="img-box">
+        <img :src="livro.capa" alt="Imagem" />
+      </div>
+      <div class="content">
+        <div class="title-background">
+          <h2>{{ livro.titulo }}</h2>
         </div>
+        <p>aaaa</p>
+        <a href="">Read More</a>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped lang="css">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap');
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+}
+
+.container {
+    position: relative;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 110px 50px;
+    padding: 50px 50px;
+}
+
+.container .card {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    width: 350px;
+    max-width: 100%;
+    height: 300px;
+    background: white;
+    border-radius: 20px;
+    transition: 0.5s;
+    box-shadow: 0 35px 80px rgba(0, 0, 0, 0.15);
+}
+
+.container .card:hover {
+    height: 400px;
+    cursor: pointer;
+}
+
+.container .card .img-box {
+    position: absolute;
+    justify-content: center;
+    align-items: center;
+    top: 0px;
+    width: 100%;
+    height: 100%;
+    background: #333;
+    border-radius: 12px;
+    overflow: hidden;
+    transition: 0.5s;
+    z-index: 1;
+}
+
+.container .card:hover .img-box {
+    top: -100px;
+    scale: 0.75;
+    box-shadow: 0 15px 45px rgba(0, 0, 0, 0.2);
+}
+
+.container .card .img-box img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  ;
+}
+
+.container .card .content .title-background {
+  background-color: white;
+  padding: 10px;
+  border-radius: 10px;
+  margin-bottom: 10px;
+  position: relative; 
+  z-index: 1;
+}
+
+.container .card .content {
+    position: absolute;
+    top: 252px;
+    width: 100%;
+    height: 35px;
+    padding: 0 30px;
+    text-align: center;
+    overflow: hidden;
+    transition: 0.5s;
+}
+
+.container .card:hover .content {
+    top: 250px;
+    height: 250px;
+}
+
+.container .card .content h2 {
+  font-size: 1.5rem;
+  font-weight: 700;
+  color: var(--clr);
+  margin: 0;
+}
+
+.container .card .content p {
+    color: #333;
+}
+
+.container .card .content a {
+    position: relative;
+    top: 15px;
+    display: inline-block;
+    padding: 12px 25px;
+    text-decoration: none;
+    background: var(--clr);
+    color: white;
+    font-weight: 500;
+}
+
+.container .card .content a:hover {
+    opacity: 0.8;
+}
+
+@media (max-width: 480px) {
+    .container .card {
+        width: 230px;
+        border-radius: 15px;
+    }
+
+    .container .card .img-box {
+        width: 185px;
+        border-radius: 10px;
+    }
+
+    .container .card .content p {
+        font-size: 0.8rem;
+    }
+
+    .container .card .content a {
+        font-size: 0.9rem;
+    }
+}
+</style>
+
